458d7f272ff515813732e7ce456237bd34de0d45
3721jockey <80378459+3721jockey@users.noreply.github.com>
2023-04-05 22:20:42 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello</h1>
+    <p>I am Jockey. I'm from Aluva</p>
+  </div>,
+  document.getElementById("root")
+);
